02d4212447fa26970bd8ce8424f2783830ad5ed5
ALICE ZEURGO CRUZ <23111712@dsn127m1249386.cfp127aluno.local>
2024-02-29 17:22:08 -0300

    faltando ajustes

--- app/src/main/java/br/senai/sp/jandira/bmi/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/MainActivity.kt
@@ -3,22 +3,33 @@ package br.senai.sp.jandira.bmi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +38,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.bmi.ui.theme.BmiTheme
@@ -51,43 +63,94 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting() {
    Column {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-            .background(Color(0xFFEE3434))
-            .padding(16.dp)){
-                  Image(painter = painterResource(id = R.drawable.bmi),
-                      contentDescription = "imc",
-                      modifier = Modifier.padding(vertical = 20.dp, horizontal = 129.dp))
+       Card {
+           Column (
+               modifier = Modifier
+                   .background(color = Color(0xFFB81010))
+                   .width(3000.dp)
+                   .height(200.dp)
+           ){
+            Card (
+                shape = CircleShape,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
 
+
+            ){
+                Card (
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .size(90.dp)
+                ){
+
+                    //Adiciona a imagem dentro do card!! (Como por exemplo para icone de perfil do usuario)
+                    Image(painter = painterResource(id = R.drawable.bmi)
+                        , contentDescription = "BMI."
+                    )
+                    Text(text = "Calculadora IMC",
+                        color = Color.White,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.W900,
+                        modifier = Modifier
+                            .offset(x = 10.dp, y = 10.dp )
+                    )
+                }
+            }
            }
        }
-    Column (
-        modifier = Modifier
-            .padding(horizontal = 129.dp,
-                vertical = 150.dp)
-    ) {
-        Text(
-            text = "Calculadora IMC",
-            fontSize = 17.sp,
-            fontWeight = FontWeight.W900,
-            color = Color.White
-        )
-    }
-    Column(
-        modifier = Modifier
-            .padding(horizontal = 135.dp,
-                vertical = 190.dp)
-            .width(14000.dp)
-            .height(1400.dp)
-            .background(Color(0xFFF5EEEE))
-                )
-    {
-      {
+       Card (
+           modifier = Modifier
+               .padding(vertical = 20.dp)
+               .align(
+                   Alignment.CenterHorizontally,
+               )
+       ){
+           Column (
+               modifier = Modifier
+                   .padding(10.dp)
+           ){
+               Text(text = "Seus dados",
+                   color = Color(0xFFB60000),
+                   modifier = Modifier
 
-        }
-    }
+                   )
+               Column {
+                   Text(text = "Seu peso:")
+                   OutlinedTextField(value = "Seu peso em kg", onValueChange = {})
+               }
+               Column (
+                   modifier = Modifier
+                       .padding(10.dp)
+               ) {
+                   Text(text = "Sua altura:")
+                   OutlinedTextField(value = "Sua altura em cm", onValueChange = {})
+               }
+               Button(onClick = {}) {
+                   Text(text = "CALCULAR")
+               }
+           }
+       }
+       Card (
+           modifier = Modifier
+               .background(Color(0xFF40A249)),
+
+       ){
+           Row (
+               modifier = Modifier
+                   .background(Color(0xFF40A249))
+           ){
+                Column {
+                    Text(text = "Resultado")
+                    Text(text = "Peso Ideal")
+                }
+               Text(text = "21.3")
+           }
+       }
+
    }
+}
+
+
 
 
 
@@ -99,3 +162,30 @@ fun GreetingPreview() {
         Greeting()
     }
 }
+
+// Column (
+//                  modifier = Modifier
+//                      .fillMaxWidth()
+//                      .height(200.dp),
+//                  horizontalAlignment = Alignment.CenterHorizontally,
+//                  verticalArrangement = Arrangement.SpaceEvenly,
+//
+//                  ){
+//                  TextField(value = "", onValueChange = {},
+//                      placeholder = {
+//                          Text(
+//                              text = "Seu peso em kg"
+//                          )
+//
+//                      },
+//                      colors = OutlinedTextFieldDefaults
+//                          .colors(
+//                              focusedBorderColor = Color(0xFFed145b),
+//                              unfocusedBorderColor = Color(0xffed145b)
+//                          ),
+//                      shape = RoundedCornerShape(12.dp),
+//                      modifier = Modifier
+//                          .height(50.dp)
+//
+//                  )
+//              }
