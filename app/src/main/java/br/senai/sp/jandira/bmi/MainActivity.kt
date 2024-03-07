4c31f837891c1db0dd2af41e5fe0ef7a693723ba
ALICE ZEURGO CRUZ <23111712@dsn127m1249386.cfp127aluno.local>
2024-03-07 15:59:22 -0300

    Faltando ajustes em detalhes

--- app/src/main/java/br/senai/sp/jandira/bmi/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -22,6 +23,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +36,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,6 +46,7 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.bmi.ui.theme.BmiTheme
+import java.util.concurrent.RecursiveTask
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,15 +72,15 @@ fun Greeting() {
            Column (
                modifier = Modifier
                    .background(color = Color(0xFFB81010))
-                   .width(3000.dp)
-                   .height(200.dp)
+                   .fillMaxWidth()
+                   .height(180.dp),
+                  horizontalAlignment = Alignment.CenterHorizontally
            ){
             Card (
                 shape = CircleShape,
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
-
-
+                    .padding(vertical = 25.dp)
             ){
                 Card (
                     modifier = Modifier
@@ -89,10 +94,7 @@ fun Greeting() {
                     )
                     Text(text = "Calculadora IMC",
                         color = Color.White,
-                        fontSize = 25.sp,
-                        fontWeight = FontWeight.W900,
-                        modifier = Modifier
-                            .offset(x = 10.dp, y = 10.dp )
+                        fontSize = 25.sp
                     )
                 }
             }
@@ -100,60 +102,98 @@ fun Greeting() {
        }
        Card (
            modifier = Modifier
+               .height(460.dp)
+               .padding(horizontal = 40.dp)
                .padding(vertical = 20.dp)
+               .offset(x = 0.dp, y = -50.dp)
                .align(
                    Alignment.CenterHorizontally,
                )
        ){
            Column (
                modifier = Modifier
-                   .padding(10.dp)
+                   .padding(10.dp),
+               horizontalAlignment = Alignment.CenterHorizontally
+
            ){
                Text(text = "Seus dados",
                    color = Color(0xFFB60000),
-                   modifier = Modifier
+                   fontSize = 25.sp,
+                   fontWeight = FontWeight.ExtraBold
 
                    )
-               Column {
-                   Text(text = "Seu peso:")
+               Column (
+                   modifier = Modifier
+                       .padding(vertical = 50.dp)
+               ){
+                   Text(text = "Seu peso:",
+                       color = Color(0xffb60000)
+                   )
                    OutlinedTextField(value = "Seu peso em kg", onValueChange = {})
                }
                Column (
                    modifier = Modifier
                        .padding(10.dp)
                ) {
-                   Text(text = "Sua altura:")
+                   Text(text = "Sua altura:",
+                       color = Color(0xffb60000))
                    OutlinedTextField(value = "Sua altura em cm", onValueChange = {})
                }
-               Button(onClick = {}) {
-                   Text(text = "CALCULAR")
+               Button(onClick = {},
+                   modifier = Modifier
+                       .width(300.dp)
+                       .height(50.dp),
+                      elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp),
+                   colors = ButtonDefaults.buttonColors(
+                       containerColor = Color(0xffDA0F0F)
+                   )
+                   ) {
+                   Text(text = "CALCULAR",
+                       )
                }
            }
        }
-       Card (
+
+       Box(
            modifier = Modifier
-               .background(Color(0xFF40A249)),
+               .offset(x = 0.dp, y = 55.dp)
+               .width(320.dp)
+               .height(120.dp)
+               .offset(x = 40.dp, y = -105.dp)
+               .background(color = Color(0xFF2D6D1F)
+               )
 
        ){
-           Row (
-               modifier = Modifier
-                   .background(Color(0xFF40A249))
-           ){
-                Column {
-                    Text(text = "Resultado")
-                    Text(text = "Peso Ideal")
-                }
-               Text(text = "21.3")
+           Row {
+               Column(
+                   horizontalAlignment = Alignment.CenterHorizontally,
+                   modifier = Modifier
+                       .fillMaxHeight()
+                       .padding(25.dp, 25.dp)
+               ) {
+                   Text(
+                       text = "Resultado",
+                       color = Color.White,
+                       fontSize = 15.sp
+                   )
+                   Text(
+                       text = "Peso Ideal!",
+                       color = Color.White,
+                       fontSize = 25.sp
+                   )
+               }
+               Text(
+                   text = "21.3",
+                   color = Color.White,
+                   fontSize = 40.sp,
+                   modifier = Modifier.offset(x = 30.dp, y = 30.dp)
+               )
+
            }
        }
 
    }
 }
-
-
-
-
-
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
